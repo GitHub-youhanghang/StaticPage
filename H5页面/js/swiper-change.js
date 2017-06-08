fa1475014d71c33186874460736fcc32741e6a4a
GitHub-youhanghang <youhanghang9527@gmail.com>
2017-06-08 18:11:55 +0800

    dd

---
+++ H5页面/js/swiper-change.js
@@ -0,0 +1,9 @@
+var swiper = new Swiper('.swiper-container', {
+    nextButton: '.swiper-button-next',
+    prevButton: '.swiper-button-prev',
+    paginationClickable: true,
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: 2500,
+    autoplayDisableOnInteraction: false
+});
